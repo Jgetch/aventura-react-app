57444e0654c1918ba092b5d1aa3dee0074e6acdb
Jgetch <115777900+Jgetch@users.noreply.github.com>
2023-03-28 16:38:42 +0100

    feature: added props into card

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import './App.css';
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero'
-import Card from './components/Card'
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Hero/>
-     <Card/>
+      <Navbar />
+      <Hero />
+      <Card
+        img="katie-zaferes.png"
+        star="./assets/Star.png"
+        rating={4.5}
+        reviewCount={341}
+        country="USA"
+        title="How to keep motivated with Katie Zaferes"
+        price={44}
+      />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-function Card() {
-    return (
-        <div className = "card">
-            <img className = "card--image" src="../assets/katie-zaferes.png" />
-            <div className = 'card--stats'>
-                <img className = 'card--star' src="../assets/star.png"/>
-                <span>5.0</span>
-                <span>(6 ratings)•</span>
-                <span>USA</span>
-            </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className ='bold'>From £80</span> per person</p>
-        </div>
-    )
+function Card(props) {
+  return (
+    <div className="card">
+      <img className="card--image" src={`./assets/${props.img}`} />
+      <div className="card--stats">
+        <img className="card--star" src={props.star} />
+        <span>{props.rating}&nbsp;</span>
+        <span>({props.reviewCount}) -&nbsp;</span>
+        <span>{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p>
+        <span className="bold">From £{props.price}</span> per person
+      </p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
