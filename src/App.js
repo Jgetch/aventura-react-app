f02a60d028bc954cadbe3caa81e51045c0f787df
Jgetch <115777900+Jgetch@users.noreply.github.com>
2023-03-31 14:34:49 +0100

    broke card elements into it's own component for reusability

--- src/App.js
+++ src/App.js
@@ -2,28 +2,14 @@ import "./App.css";
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Card from "./components/Card";
-import mockData from "./mockData";
+import CardElements from "./components/CardElements";
 
 function App() {
-  const cards = mockData.map((item) => {
-    return (
-      <Card
-        img={item.coverImg}
-        star="./assets/Star.png"
-        rating={item.stats.rating}
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-      />
-    );
-  });
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      {cards}
+      <CardElements />
     </div>
   );
 }
